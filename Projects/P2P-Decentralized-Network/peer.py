7a1ec87f2c1bf5e7b4dbabfb13ce2748b01e2436
Amir <ar.anjomshoaa@gmail.com>
2020-05-16 18:56:34 -0700

    setting up tracker announce

--- Projects/P2P-Decentralized-Network/peer.py
+++ Projects/P2P-Decentralized-Network/peer.py
@@ -156,7 +156,7 @@ class Peer(Server,Client):
                     # now do something with the clientsocket
                     # in this case, we'll pretend this is a threaded server
                     Thread(target=self.peer_handler, args=(server,clientsocket, address)).start()
-                    data = {'clientid': self.client_id}
+                    data = {'clientid': address[1]}
                     server._send(clientsocket,data)
                     print("Client: " + str(address[1]) + " just connected")
                 except Exception as error:
