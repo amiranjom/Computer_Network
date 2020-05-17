894ddbb9fcdf3ca5081adb2e0b58b1affb477815
Amir <ar.anjomshoaa@gmail.com>
2020-05-17 12:27:03 -0700

    testin

--- Projects/P2P-Decentralized-Network/tracker.py
+++ Projects/P2P-Decentralized-Network/tracker.py
@@ -15,6 +15,11 @@ class Tracker(object):
         print(swarm)
         print(self.swarm)
         print(swarm.get_peers())
+        for i in self.swarm:
+            if swarm.get_file_name() == i.get_file_name():
+                print("already exist")
+            else:
+                print("does not exist")
 """
         for s in self.swarm:
             if swarm.get_file_name() == s.get_file_name():
